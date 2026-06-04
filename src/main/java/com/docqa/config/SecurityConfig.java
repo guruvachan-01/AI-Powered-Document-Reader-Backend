68f70589f2813f7db70e96707723d118403aa168
guruvachan-01 <156888776+guruvachan-01@users.noreply.github.com>
2026-06-04 11:37:04 +0530

    security configf

--- src/main/java/com/docqa/config/SecurityConfig.java
+++ src/main/java/com/docqa/config/SecurityConfig.java
@@ -48,6 +48,7 @@ public class SecurityConfig {
 
                         .requestMatchers(
                                 "/auth/**",
+                                "/api/auth/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
                                 "/v3/api-docs/**",
